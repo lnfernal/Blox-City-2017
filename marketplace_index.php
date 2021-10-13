c9c2a8839b7b49476b21f885bf13aa8d2cf25a59
Mad <91134167+MadBeta@users.noreply.github.com>
2021-10-13 08:46:23 +0700

    Create marketplace_index.php

---
+++ marketplace_index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>BLOX City</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://www.bloxcity.com/assets/js/general.js"></script>
+<script src="https://www.bloxcity.com/assets/js/dropdown-menu.js"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+<link rel="stylesheet" href="https://www.bloxcity.com/assets/css/style.css">
+<link rel="stylesheet" href="https://www.bloxcity.com/assets/css/dropdown-menu.css">
+<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-78458167-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+ 
+<script type="text/javascript">
+		_atrk_opts = { atrk_acct:"xrfin1aMp410mh", domain:"bloxcity.com",dynamic: true};
+		(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+		</script>
+<noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=xrfin1aMp410mh" style="display:none" height="1" width="1" alt=""/></noscript>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+		  (adsbygoogle = window.adsbygoogle || []).push({
+			google_ad_client: "ca-pub-5035877450680880",
+			enable_page_level_ads: true
+		  });
+		</script>
+ 
+</head>
+<body>
+<div class="side-bar" style="z-index:100;">
+<div class="side-bar-push"></div>
+<a href="https://www.bloxcity.com/">
+<div class="icon"><i class="material-icons">home</i></div>
+<span>Home</span>
+</a>
+<a href="https://www.bloxcity.com/create/">
+<div class="icon"><i class="material-icons">add_box</i></div>
+<span>Create</span>
+</a>
+<a href="https://www.bloxcity.com/search/">
+<div class="icon"><i class="material-icons">search</i></div>
+<span>Search</span>
+</a>
+<a href="https://www.bloxcity.com/marketplace/">
+<div class="icon"><i class="material-icons">invert_colors</i></div>
+<span>Marketplace</span>
+</a>
+<a href="https://www.bloxcity.com/upgrade/">
+<div class="icon"><i class="material-icons">shopping_basket</i></div>
+<span>Upgrade</span>
+</a>
+<div class="small-push"></div>
+<div class="bar-divider"></div>
+<div class="small-push"></div>
+<a href="https://www.bloxcity.com/account/inbox/" style="position:relative;">
+<div class="icon"><i class="material-icons">message</i></div>
+<span>Inbox</span>
+</a>
+<a href="https://www.bloxcity.com/account/friends/" style="position:relative;">
+<div class="icon"><i class="material-icons">person</i></div>
+<span>Friends</span>
+</a>
+<a href="https://www.bloxcity.com/account/trades/" style="position:relative;">
+<div class="icon"><i class="material-icons">swap_vert</i></div>
+<span>Trades</span>
+</a>
+<div class="small-push"></div>
+<div class="bar-divider"></div>
+</div>
+<div class="site-parent">
+<div class="site-content">
+<div class="header-top" style="overflow:hidden;">
+<div class="primary-logo"></div>
+<div class="container-header">
+<div style="float:left;">
+<a href="https://www.bloxcity.com"><img src="https://cdn.bloxcity.com/web/01_logo_site_main.png" width="154" style="position:absolute;display:inline-block;margin-top: 17.5px;margin-left:18px;"></a>
+<div style="width:225px;display:inline-block;"></div>
+<a href="https://www.bloxcity.com/games/" class="olink">
+<div class="icon"><i class="material-icons">videogame_asset</i></div>
+<span>Games</span>
+</a>
+<a href="https://www.bloxcity.com/store/" class="olink">
+<div class="icon"><i class="material-icons">store</i></div>
+<span>Store</span>
+</a>
+<a href="https://www.bloxcity.com/forum/" class="olink">
+<div class="icon"><i class="material-icons">forum</i></div>
+<span>Forum</span>
+</a>
+<a href="https://blog.bloxcity.com" target="_blank" class="olink">
+<div class="icon"><i class="material-icons">content_copy</i></div>
+<span>Blog</span>
+</a>
+</div>
+<div style="float:right;">
+<a style="cursor:pointer;position:relative;" class="olink" title="0 notifications">
+<i class="material-icons" style="margin-top:0;margin-top:-1px;vertical-align:middle;">notifications</i>
+<span style="font-size:14px;">0</span>
+</a>
+<a style="cursor:pointer;position:relative;" class="olink" title="10 Cash">
+<i class="material-icons" style="color:#4F953B;margin-top:0;font-size:22px;margin-top:-1px;vertical-align:middle;">monetization_on</i>
+<span style="font-size:14px;">10</span>
+</a>
+<div class="dropdown-parent">
+<a id="DropdownButton" style="cursor:pointer;" onclick="handleDropdown()" class="olink dropdownbutton">
+<span>
+<div style="width:42px;height:42px;border-radius:50%;border:1px solid #DEDEDE;background:url(https://cdn.bloxcity.com/8ca17bec-0320-4293-90e5-dfc5b8690156-thumb.png);background-size:cover;"></div>
+</span>
+</a>
+<div id="MenuDropdown" class="top-bar-dropdown">
+<div class="top-bar-dropdown-padding-area-nb">
+<div style="width:32px;height:32px;border-radius:50%;border:1px solid #DEDEDE;background:url(https://cdn.bloxcity.com/8ca17bec-0320-4293-90e5-dfc5b8690156-thumb.png);background-size:cover;"></div>
+<span>Testaccount3432<div style="font-size:11px;font-weight:normal;display:block;color:#999;">0 Credits</div></span>
+</div>
+<div class="top-bar-dropdown-padding-area">
+<a href="https://www.bloxcity.com/users/Testaccount3432/" style="border-top:0;">Profile</a>
+<a href="https://www.bloxcity.com/account/character/">Character</a>
+</div>
+<div class="top-bar-dropdown-padding-area">
+<a href="https://www.bloxcity.com/account/settings/">Settings</a>
+<a href="https://www.bloxcity.com/account/logout/">Logout</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="page-push"></div>
+<div class="container-parent" style="padding-left: 225px;">
+<div class="container container-special">
+<div class="center-align">
+<div style="margin-bottom:50px;">
+<div class="text-center">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+ 
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5035877450680880" data-ad-slot="8645193052" data-ad-format="auto"></ins>
+<script>
+							(adsbygoogle = window.adsbygoogle || []).push({});
+							</script>
+</div>
+</div>
+</div>
+<script>
+		document.title = "Marketplace | BLOX City";
+	</script>
+<div class="lg-container center games-construction">
+<i class="material-icons">timelapse</i>
+<div>The marketplace feature is coming soon.<br/>Check back soon!</div>
+</div>
+<div style="height:50px;"></div>
